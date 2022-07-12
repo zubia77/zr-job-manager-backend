81f32762b49d068f5cab46d7758a547bb72150aa
Zubia <rashzubia@gmail.com>
2022-07-12 11:03:50 +0200

    letzten 2 einchecken

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3044;
  
 app.get('/', (req, res) => {
-    res.send('<h1>Book Site API</h1>');
+    res.send('<h1>ZR Job Manager API</h1>');
 });
  
 app.listen(port, () => {
