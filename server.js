0146be93b7c09a0e5606719b826694b8eca407a9
Zubia <rashzubia@gmail.com>
2022-07-12 11:35:01 +0200

    ready to test the

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 import express from 'express';
+import { JobSource } from './models/JobSource.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const MONGODB_URI = 'mongodb://localhost/zr-job-manager';
+
+mongoose.connect(MONGODB_URI, (err) => {
+    if (err) {
+        console.log({
+            error: "Cannot connect to MongoDB database.",
+            err: `"${err}"`
+        });
+    }
+});
  
 const app = express();
 const port = process.env.PORT || 3044;
