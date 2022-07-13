8d21044a91823e17e191c85f31ff375b31315af0
Zubia <rashzubia@gmail.com>
2022-07-13 13:45:07 +0200

    jwt

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { JobSource } from './models/JobSource.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors'
+import jwt from 'jsonwebtoken';
 
 const user = {
     id: 1,
@@ -38,8 +39,11 @@ app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (username === 'hans' && password === '123') {
-        res.json({
-            user
+        jwt.sign({ user }, 'secretkey', { expiresIn: '20s' }, (err, token) => {
+            res.json({
+                user,
+                token
+            });
         });
     } else {
         res.sendStatus(403);
